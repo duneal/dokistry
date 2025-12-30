dfffaad3320d130a6e77943accfe9327f7583bcf
Quentin Macheda <quentin@duneal.com>
2025-12-30 11:58:18 +0100

    chore: upgrade to next.js v16 + setup nextjs mcp

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { type NextRequest, NextResponse } from "next/server"
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
 	const response = NextResponse.next()
 
 	const locale = request.cookies.get("NEXT_LOCALE")?.value || "en"
